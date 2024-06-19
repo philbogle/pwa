bddeaed4cc71f651f0a324f6cf72bd832bd385e1
philbogle <philbogle@penguin>
2024-06-19 15:50:49 -0700

    tweak

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Register service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/pwa/service-worker.js')
             .then(registration => {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             })
