c4b937773b78ca3c04cacc49ab221110977ccd53
philbogle <philbogle@penguin>
2024-06-19 15:40:52 -0700

    better menu

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.getElementById('mobile-menu');
-    const nav = document.querySelector('nav ul.nav');
+    const navMenu = document.getElementById('nav-menu');
     const scheduleLink = document.getElementById('schedule-link');
     const cabinsLink = document.getElementById('cabins-link');
     const scheduleSection = document.getElementById('schedule');
     const cabinsSection = document.getElementById('cabins');
 
     mobileMenu.addEventListener('click', () => {
-        nav.classList.toggle('active');
+        navMenu.classList.toggle('active');
     });
 
     scheduleLink.addEventListener('click', () => {
         scheduleSection.classList.add('active');
         cabinsSection.classList.remove('active');
-        nav.classList.remove('active');
+        navMenu.classList.remove('active');
     });
 
     cabinsLink.addEventListener('click', () => {
         cabinsSection.classList.add('active');
         scheduleSection.classList.remove('active');
-        nav.classList.remove('active');
+        navMenu.classList.remove('active');
     });
 
     // Show schedule by default
